45afff63b641b3e369e66bc7374a9f94cc03143a
hsq777 <email@example.com>
2018-10-31 14:11:50 +0800

    index1.0.1

--- js/index.js
+++ js/index.js
@@ -7,12 +7,9 @@
 // window.addEventListener('resize', resizeFontsize);
 $(document).ready(function () {
     var swiper = new Swiper('.swiper-container', {
-        // pagination: '.swiper-pagination',
-        direction: 'vertical',
-        slidesPerView: 1,
+        pagination: '.swiper-pagination',
         paginationClickable: true,
-        spaceBetween: 30,
-        mousewheelControl: true,
+        direction: 'vertical',
         onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
         swiperAnimateCache(swiper); //隐藏动画元素 
         swiperAnimate(swiper); //初始化完成开始动画
